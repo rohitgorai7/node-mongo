5e28d592dd8e3d598812fc7340fadf59f4833e44
rohit77 <rohit.webmobinfo.ch>
2023-03-31 23:05:15 +0530

    update-delete-product

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const { DB_NAMES, PRODUCTS_MESSAGES } = require('./consts');
+const { DB_NAMES, PRODUCTS_MESSAGES, ACTIONS } = require('./consts');
 const Product = require('./models/productModel');
 
 //to use ObjectId in mongoose find
@@ -11,7 +11,7 @@ const ObjectId = mongoose.Types.ObjectId;
 //to use json in post, put
 app.use(express.json());
 //to use form data in post, put
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 //routes
 
 app.get('/', (req, res) => {
@@ -24,15 +24,16 @@ app.get('/blog', (req, res) => {
 
 //products routes
 //get all products
-app.get('/get-products',async (req, res) => {
+app.get('/get-products', async (req, res) => {
     try {
         const products = await Product.find({});
         const response = {
             products: products,
             message: PRODUCTS_MESSAGES.GET_PRODUCTS
         }
-        res.status(200).json(response);        
+        res.status(200).json(response);
     } catch (error) {
+        console.log(error);
         res.status(500).json({message: error.message});
     }
 });
@@ -52,6 +53,7 @@ app.get('/get-product/:id',async (req, res) => {
             res.status(200).json(response);
         }
     } catch (error) {
+        console.log(error);
         res.status(500).json({message: error.message});
     }
 });
@@ -61,23 +63,72 @@ app.post('/add-product',async (req, res) => {
         await Product.create(req.body);
         res.status(200).json({message: PRODUCTS_MESSAGES.ADD_PRODUCT});
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
         res.status(500).json({message: error.message});
     }
 });
-//update a product by id
-app.put('/update-product/:id',async (req, res) => {
+//update and delete a product by id
+// app.post('/update-product/:id',async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const action = req.body.action;
+//         let product;
+//         if(action === ACTIONS.UPDATE) {
+//             const payload = {
+//                 name: req.body.name,
+//                 quantity: req.body.quantity,
+//                 price: req.body.price,
+//                 image: req.body.image
+//             }
+//             product = await Product.findByIdAndUpdate(id, payload);
+//         } else if(action === ACTIONS.DELETE) {
+//             product = await Product.findByIdAndDelete(id);
+//         } else {
+//             return res.status(404).json({message: 'Unidentified action'});
+//         }
+//         if(!product) {
+//             return res.status(404).json({message: `cannot find product with id - ${id}`});
+//         }
+//         const o_id = new ObjectId(id);
+//         const updatedProduct = await Product.findOne({_id: o_id});
+//         const response = {
+//             ...(action === ACTIONS.UPDATE ? { message: `Product updated successfully`, product: updatedProduct } : action === ACTIONS.DELETE ? { message: `Product deleted successfully` } : {})
+//         }
+//         res.status(200).json(response);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({message: error.message});
+//     }
+// });
+app.post('/update-product',async (req, res) => {
     try {
-        const { id } = req.params;
-        const  product = await Product.findByIdAndUpdate(id, req.body);
+        const id = req.body._id;
+        const action = req.body.action;
+        let product;
+        if(action === ACTIONS.UPDATE) {
+            const payload = {
+                name: req.body.name,
+                quantity: req.body.quantity,
+                price: req.body.price,
+                image: req.body.image
+            }
+            product = await Product.findByIdAndUpdate(id, payload);
+        } else if(action === ACTIONS.DELETE) {
+            product = await Product.findByIdAndDelete(id);
+        } else {
+            return res.status(404).json({message: 'Unidentified action'});
+        }
         if(!product) {
             return res.status(404).json({message: `cannot find product with id - ${id}`});
         }
         const o_id = new ObjectId(id);
-        const updatedProduct = await Product.findOne({_id: o_id}); 
-        res.status(200).json({message: `Product-${updatedProduct} updated successfully`});
+        const updatedProduct = await Product.findOne({_id: o_id});
+        const response = {
+            ...(action === ACTIONS.UPDATE ? { message: `Product updated successfully`, product: updatedProduct } : action === ACTIONS.DELETE ? { message: `Product deleted successfully` } : {})
+        }
+        res.status(200).json(response);
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
         res.status(500).json({message: error.message});
     }
 });
